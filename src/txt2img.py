110f0db40fadcc9c68db209756b804d2d81e1ecf
CapsAdmin <eliashogstvedt@gmail.com>
2023-07-24 20:43:31 +0200

    placeholders for remaining options

--- src/txt2img.py
+++ src/txt2img.py
@@ -15,7 +15,7 @@ def txt2img(
     seed = 0,
     width = 512,
     height = 512,
-    eta_noise_seed_delta = 31337,
+    
     batch_size = 1,
     cfg_scale = 7,
     subseed_strength = 0,
@@ -23,17 +23,58 @@ def txt2img(
     seed_resize_from_h = 0,
     seed_resize_from_w = 0,
     clip_skip = 1,
-
-    # sampler args from a1111
-    enable_quantization = True,
-    use_old_karras_scheduler_sigmas = False,
-    always_discard_next_to_last_sigma = True,
+    
+    # Which algorithm is used to produce the image
     sampler_name = "DPM++ 2M SDE",
+
+    # Enable quantization in K samplers for sharper and cleaner results. This may change existing seeds. Requires restart to apply.
+    enable_quantization = True,
+
+    # Use old karras scheduler sigmas (0.1 to 10).
+    use_old_karras_scheduler_sigmas = False,
+
+    # Do not make DPM++ SDE deterministic across different batch sizes.
+    dont_fix_dpmpp_sde_batch_size = False,
+
+    # Do not fix prompt schedule for second order samplers.
+    dont_fix_prompt_schedule = False,
+
+    # Make K-diffusion samplers produce same images in a batch as when making a single image
+    k_diffusion_batch = True,
+
+    # Random number generator source. (changes seeds drastically; use CPU to produce the same picture across different videocard vendors)
+    random_source = "gpu",
+
+    # sampler parameters from a1111
+    sigma_churn = 0.0,
+    sigma_tmin = 0.0,
+    sigma_noise = 1.0,
+
     # scheduler type (lets you override the noise schedule for k-diffusion samplers; choosing Automatic disables the three parameters below)
     k_sched_type = "Automatic",
-    rho = 1.0,
+    
+    # sigma min (0 = default (~0.03); minimum noise strength for k-diffusion noise scheduler)
     sigma_min = 0,
+
+    # sigma max (0 = default (~14.6); maximum noise strength for k-diffusion noise schedule)
     sigma_max = 0,
+
+    #  rho (0 = default (7 for karras, 1 for polyexponential); higher values result in a more steep noise schedule (decreases faster))
+    rho = 1.0,
+
+    # Eta noise seed delta (ENSD; does not improve anything, just produces different results for ancestral samplers - only useful for reproducing images)
+    eta_noise_seed_delta = 31337,
+
+    # Always discard next-to-last sigma https://github.com/AUTOMATIC1111/stable-diffusion-webui/pull/6044
+    always_discard_next_to_last_sigma = True,
+
+    unipc_variant = "bh1", # bh2, vary_coef
+    unipc_skip_type = "time_uniform", # time_quadratic logSNR
+
+    # UniPC order (must be < sampling steps)
+    unipc_order = 3,
+
+    unipc_lower_order_final = True,
 ):
     pipe = StableDiffusionPipeline.from_single_file(
         resources.checkpoints[checkpoint], 
